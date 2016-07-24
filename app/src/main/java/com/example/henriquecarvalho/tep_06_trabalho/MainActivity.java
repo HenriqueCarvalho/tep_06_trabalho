e4c5e8d8713a710e4855819b1d00da322afac8d8
henriquecarvalho <henrique.o.carvalho@hotmail.com.br>
2016-07-23 23:29:37 -0300

    start project

--- app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/MainActivity.java
+++ app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.henriquecarvalho.tep_06_trabalho;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Minhas Viagens test");
+        actionBar.setLogo(R.drawable.ic_action_share);
+        //actionBar.setHomeAsUpIndicator(R.drawable.ic_action_share);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        /*ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Minhas Viagens");
+        actionBar.setLogo(R.drawable.ic_action_share);
+        actionBar.setDisplayShowHomeEnabled(true);*/
+    }
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Infla o menu com os botões da action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
