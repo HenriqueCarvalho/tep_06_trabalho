3c51a626c87b08ed889a66b47db459292d7cf1bf
henriquecarvalho <henrique.o.carvalho@hotmail.com.br>
2016-07-27 05:56:51 -0300

    Update classes and Add delete costs

--- app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/CostAdapter.java
+++ app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/CostAdapter.java
@@ -1,26 +1,32 @@
 package com.example.henriquecarvalho.tep_06_trabalho;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by henriquecarvalho on 2016-07-26.
  */
-public class CostAdapter  extends BaseAdapter {
+public class CostAdapter extends BaseAdapter {
+
+    private static final String TAG = CostAdapter.class.getSimpleName();
     private final Context context;
     private final List<Cost> costs;
+    final List<Cost> seletedItens;
+
 
     public CostAdapter(Context context, List<Cost> costs) {
         this.context = context;
         this.costs = costs;
+        this.seletedItens = new ArrayList<Cost>();
     }
 
     @Override
@@ -38,13 +44,40 @@ public class CostAdapter  extends BaseAdapter {
         return position;
     }
 
+    public void removeSelectedItens() {
+
+        for (int i = 0; i < seletedItens.size(); i++) {
+            costs.remove(seletedItens.get(i));
+        }
+
+        notifyDataSetChanged();
+    }
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
         // Infla a view
         View view = LayoutInflater.from(context).inflate(R.layout.list_costs, parent, false);
 
+        final int pos = position;
+
         // Faz findViewById das views que precisa atualizar
         CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+
+        checkBox.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View view) {
+                CheckBox cb = (CheckBox) view ;
+                if (cb.isChecked()) {
+                    seletedItens.add(costs.get(position));
+                    Log.d(TAG, "onClick()"+seletedItens.toString());
+                } else {
+                    if (seletedItens.contains(costs.get(position))) {
+                        seletedItens.remove(costs.get(position));
+                        Log.d(TAG, "remove()"+seletedItens.toString());
+                    }
+                }
+            }
+        });
+
         TextView typeOfCost = (TextView) view.findViewById(R.id.typeOfCostTextView);
         TextView date = (TextView) view.findViewById(R.id.dateTextView);
         TextView price = (TextView) view.findViewById(R.id.priceTextView);
--- app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/MyCostsActivity.java
+++ app/src/main/java/com/example/henriquecarvalho/tep_06_trabalho/MyCostsActivity.java
@@ -1,9 +1,16 @@
 package com.example.henriquecarvalho.tep_06_trabalho;
 
+import android.content.DialogInterface;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -13,6 +20,7 @@ public class MyCostsActivity extends AppCompatActivity {
 
     private int id = 0;
     private ListView listView;
+    private CostAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,7 @@ public class MyCostsActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             int position = extras.getInt("position");
+
             // If you want to get string
             //String someVariable = extras.getString("someVariable");
 
@@ -37,10 +46,67 @@ public class MyCostsActivity extends AppCompatActivity {
                 Log.d(TAG, "onCreate()"+c.getType().toString());
             }
 
-            listView.setAdapter(new CostAdapter(this, costs));
+            adapter = new CostAdapter(this, costs);
 
-            // listView.setOnItemClickListener(this);
-
+            listView.setAdapter(adapter);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my_costs, menu);
+        MenuItem deleteViewItem = menu.findItem(R.id.action_delete);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_delete:
+                if(adapter.seletedItens.isEmpty()) {
+                    Toast.makeText(this, "Não possui gastos selecionados!", Toast.LENGTH_SHORT).show();
+                } else {
+                    alertMessage();
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void alertMessage() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE: // Yes button clicked
+                        adapter.removeSelectedItens();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE: // No button clicked
+                        // do nothing
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Você deseja deletar os gastos selecionados?")
+                .setPositiveButton("Sim", dialogClickListener)
+                .setNegativeButton("Não", dialogClickListener).show();
+    }
+
 }
